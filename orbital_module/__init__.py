71dd6e5aed21879eab2f66f900557450f32b82c0
javig <javier.garrido.castillo@gmail.com>
2024-02-23 11:34:00 +0100

    Update version to 2.3: new umbra function

--- orbital_module/__init__.py
+++ orbital_module/__init__.py
@@ -4,4 +4,4 @@ from utils.earth_station import EarthStation
 from utils.orientation import satellite_orientation
 from utils.utils import rv_to_nu, R_x, R_y, R_z, R_euler_zxz
 
-__version__ = '2.2'
+__version__ = '2.3'
